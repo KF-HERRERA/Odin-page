3374aad5f9f6591d47d554f5c79b9b1b11b2c459
G4T00 <keni-266@hotmail.com>
2023-12-11 12:55:02 -0500

    end responsive

--- js/main.js
+++ js/main.js
@@ -17,3 +17,18 @@ document.addEventListener("DOMContentLoaded", function () {
 
     window.addEventListener("scroll", handleScroll);
   });
+
+document.querySelector(".bars_menu").addEventListener("click", animateBars);
+
+var line1_bars = document.querySelector(".line1-bars-menu");
+var line2_bars = document.querySelector(".line2-bars-menu");
+var line3_bars = document.querySelector(".line3-bars-menu");
+// var despleg = document.getElementById("active");
+
+function animateBars() {
+    line1_bars.classList.toggle("activeline1-bars-menu");
+    line2_bars.classList.toggle("activeline2-bars-menu");
+    line3_bars.classList.toggle("activeline3-bars-menu");
+    // despleg.classList.toggle("active-fixed");
+
+}
